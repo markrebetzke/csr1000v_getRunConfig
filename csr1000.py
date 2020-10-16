dc1f09e76fee4355ebbbee3b3c51905927c0436a
mark.rebetzke <markrebetzke@gmail.com>
2020-10-16 13:22:59 +1100

    fixing comments

--- csr1000.py
+++ csr1000.py
@@ -4,7 +4,7 @@ from requests.auth import HTTPBasicAuth
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-## Adding global variables
+## Adding global attributes
 
 roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vZWI4MDU0YzAtM2NkOC0xMWVhLThhMTQtNTlkNTU4ZjFhMmI0"
 webexToken = "YjIwMjRhMmItOGRhYi00MGE4LTk5OTctZDRhNDcwZDA4NzMwMDY3MGYwZTktZjJi_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
